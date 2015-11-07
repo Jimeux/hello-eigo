97dfb91bce053ed78973b3695bbacf0471931aec
James Kirk <jimeux@gmail.com>
2015-11-07 22:08:58 +0900

    Improve JPN translation

--- app/src/debug/java/com/moobasoft/damego/DebugApp.java
+++ app/src/debug/java/com/moobasoft/damego/DebugApp.java
@@ -1,17 +1,15 @@
 package com.moobasoft.damego;
 
-import com.facebook.stetho.Stetho;
-
 public class DebugApp extends App {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Stetho.initialize(Stetho.newInitializerBuilder(this)
+        /*Stetho.initialize(Stetho.newInitializerBuilder(this)
                 .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
                 .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
-                .build());
+                .build());*/
 
         //LeakCanary.install(this);
     }
